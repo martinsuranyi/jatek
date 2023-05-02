aa7b1b26a04ab35a6931f59f97018b11cb26a635
Kancso123 <118981104+Kancso123@users.noreply.github.com>
2023-05-02 22:12:00 +0200

    main ujkafa

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 import random
 
-
-def main():
-    print("Üdvözöllek a Robot szólánc játékban!")
-    print("A játék lényege, hogy a megadott szó utolsó betűjével kell kezdened a következő szót.")
-    print("Ha nem tudsz szót mondani, vesztettél.")
-    print("Kezdjük!")
-
 def main():
     words = ["alma", "baba", "csokoládé", "dió", "eper", "fűszer", "gumicsizma", "hinta", "iskola", "jégkorong", "karfiol", "limonádé", "málna", "narancs", "óra", "paprika", "quiche", "rózsa", "szék", "tyúk", "uborka", "víz", "walesi", "xilofon", "yzop"]
     print("Üdvözöllek a robot szólánc játékban!")
     print("A játék célja, hogy olyan szót adj meg, amely az előző szó utolsó betűjével kezdődik.")
     user_word = input("Kezdjük! Add meg az első szót: ")
-    while user_word:
-        robot_word = [word for word in words if word[0] == user_word[-1] if len(user_word) > 0]
-        if not robot_word:
-            print("Sajnálom, nem találtam olyan szót, amely az utolsó betűvel kezdődik.")
-            break
-        robot_choice = robot_word[0]
-        print("Az én szavam:", robot_choice)
-        user_word = input("Add meg a következő szót: ")
-    print("Köszönöm, hogy játszottál!")
 
     # Betöltjük a szavakat tartalmazó fájlt.
     with open("magyarszavak.txt", "r", encoding="utf-8") as f:
@@ -29,5 +13,34 @@ def main():
     # Robot választ egy véletlen szót.
     robot_word = random.choice(words)
     print(f"A robot választott szava: {robot_word}")
-
+    felhasznált = []
     # Felhasználó választ egy szót, amely az előző szó utolsó betűjével kezdődik.
+    while True:
+        user_word = input("Kérlek írd be a következő szót: ")
+        if user_word[0] != robot_word[-1]:
+            print("Helytelen válasz! A szavad az előző szó utolsó betűjével kell kezdődjön.")
+            continue
+        if user_word in felhasznált:
+            print("Helytelen válasz! Már ezt a szót felhasználtad.")
+            continue
+        if user_word not in words:
+            print("Helytelen válasz! Az adatbázisban nincs ilyen szó.")
+            continue
+        if user_word[-1] == "s":
+            print("Helytelen válasz! Az 's' után még egy betű kell.")
+            continue
+        felhasznált.append(user_word)
+        words.remove(user_word)
+        #[word for word in words if word[0] == user_word[-1] if len(user_word)>0]
+        if not robot_word:
+            print("Gratulálok, nyertél!")
+            break
+        for e in words:
+            if e[0].capitalize() == user_word[len(user_word)-1].capitalize():
+                robot_word = e
+                break
+        print(f"A robot választott szava: {robot_word}")
+
+
+if __name__ == "__main__":
+    main()
